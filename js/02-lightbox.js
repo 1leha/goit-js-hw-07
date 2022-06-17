968d95a9b67b50984cd51dbde0e2de67aa8b5ecd
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-06-17 17:16:28 +0400

    adding 2 part of tHW7
    SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+makeGalleryMarkup(galleryItems);
+
+refs.gallery.addEventListener("click", onImageClick);
+
+let imageLightBox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function makeGalleryMarkup(gallery) {
+  refs.gallery.innerHTML = gallery
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join("");
+}
+
+function onImageClick(e) {
+  e.preventDefault();
+
+  const onEscape = (event) => {
+    if (event.key === "Escape") {
+      imageLightBox.close();
+      document.removeEventListener("keydown", onEscape);
+    }
+  };
+  document.addEventListener("keydown", onEscape);
+}
